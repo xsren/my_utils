d43bfdf6fcae4b9a92f29abab340c538d29ab758
xsren <bestrenxs@gmail.com>
2017-08-03 15:56:38 +0800

    更改pack_multiple_excel_to_one为按文件更新时间排序

--- src/pack_excel/pack_multiple_excel_to_one.py
+++ src/pack_excel/pack_multiple_excel_to_one.py
@@ -26,17 +26,25 @@ def run():
     fpath = '.'
 
     i = 1
+
+    files = []
     for fname in os.listdir(fpath):
         if fname.endswith(".xlsx"):
-            wb_r = load_workbook(filename=fname)
-            ws_r = wb_r.worksheets[0]
-            j = 1
-            print fname
+            files.append(fname)
 
-            for row in ws_r.rows:
+    # 安装最后更改时间排序，一开始用getctime但是mac上有问题（一个文件下载然后复制到别的文件夹的时候，ctime是复制的时间），于是改成getmtime
+    files.sort(key=lambda x: os.path.getmtime(x))
 
-                new_cells = []
+    for fname in files:
+        print fname
+        wb_r = load_workbook(filename=fname)
+        ws_r = wb_r.worksheets[0]
+        j = 1
+        for row in ws_r.rows:
 
+            new_cells = []
+
+            if i == 1:
                 for cell in row:
                     new_cell = WriteOnlyCell(ws_w, value=cell.value)
                     if cell.has_style:
@@ -47,17 +55,27 @@ def run():
                         new_cell.protection = copy(cell.protection)
                         new_cell.alignment = copy(cell.alignment)
                     new_cells.append(new_cell)
-
-                if i == 1:
-                    if j == 1:
-                        ws_w.title = ws_r.title
-                        ws_w.column_dimensions = ws_r.column_dimensions
+                if j == 1:
+                    ws_w.title = ws_r.title
+                    ws_w.column_dimensions = ws_r.column_dimensions
+                ws_w.append(new_cells)
+            else:
+                if j >= 4:
+                    for cell in row:
+                        new_cell = WriteOnlyCell(ws_w, value=cell.value)
+                        if cell.column == "A":
+                            new_cell.value = (i - 1) * 10 + int(cell.value)
+                        if cell.has_style:
+                            new_cell.font = copy(cell.font)
+                            new_cell.border = copy(cell.border)
+                            new_cell.fill = copy(cell.fill)
+                            new_cell.number_format = copy(cell.number_format)
+                            new_cell.protection = copy(cell.protection)
+                            new_cell.alignment = copy(cell.alignment)
+                        new_cells.append(new_cell)
                     ws_w.append(new_cells)
-                else:
-                    if j >= 4:
-                        ws_w.append(new_cells)
-                j += 1
-            i += 1
+            j += 1
+        i += 1
 
     today = time.strftime('%Y%m%d', time.localtime(time.time()))
     wb.save('new_file_%s.xlsx' % today)
